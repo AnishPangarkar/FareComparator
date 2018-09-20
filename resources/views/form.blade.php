a5939ce224547da6378f97f5d4b5cb71603cfd5d
Anish Pangarkar <apangark@edu.uwaterloo.ca>
2018-09-19 21:48:29 -0400

    front-end changes, changed index page
    front-end changes, changed index page

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,27 +1,27 @@
 @extends('app')
 @section('content')
-    <form action="/createRide" method="POST">
-        {{ csrf_field() }}
-        <div class="form-group">
-            <label for="from">From:</label>
-            <input type="text" class="form-control" id="from" name="from">
+        <div class="col-sm-3 center-div">
+            <div class="card text-center">
+                <div class="card-header">
+                    {{'Addresses'}}
+                </div>
+                <div class="card-body">
+                    <form action="/createRide" method="POST">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="from">From:</label>
+                            <input type="text" class="form-control" id="from" name="from">
+                        </div>
+                        <div class="form-group">
+                            <label for="to">To:</label>
+                            <input type="text" class="form-control" id="to" name="to">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="to">To:</label>
-            <input type="text" class="form-control" id="to" name="to">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+
 @endsection
-
-{{--@section('scripts')--}}
-    {{--<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->--}}
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>--}}
-    {{--<!-- Include all compiled plugins (below), or include individual files as needed -->--}}
-    {{--<script src="{{ asset('js/bootstrap.min.js') }}"></script>--}}
-{{--@endsection--}}
-
-
-{{--@section('footer_scripts')--}}
-    {{--<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">--}}
-{{--@endsection--}}
